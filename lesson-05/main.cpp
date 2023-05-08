9cced03e90372c25227518cfe7b5a8feeeda13e5
patrycjaprzybyszewska <126480017+patrycjaprzybyszewska@users.noreply.github.com>
2023-05-08 13:38:01 +0200

    Update main.cpp

--- lesson-05/main.cpp
+++ lesson-05/main.cpp
@@ -37,3 +37,90 @@ int main()
 
     return 0;
 }
+
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <iostream>
+#include <map>
+
+class Cantor {
+ public:
+  Cantor() = default;
+  virtual ~Cantor() = default;
+
+  virtual double Rate(const std::string& abbreviation) const = 0;
+};
+
+class FakeUsdCantor : public Cantor {
+ public:
+  FakeUsdCantor() {
+    // rates_ map with values for ten most popular currencies relative to USD
+    rates_ = {
+      {"EUR", 0.85},
+      {"JPY", 110.66},
+      {"GBP", 0.73},
+      {"CHF", 0.91},
+      {"CAD", 1.25},
+      {"AUD", 1.34},
+      {"CNY", 6.47},
+      {"HKD", 7.78},
+      {"NZD", 1.44},
+      {"KRW", 1153.89}
+    };
+  }
+
+  double Rate(const std::string& abbreviation) const override {
+    auto it = rates_.find(abbreviation);
+    if (it != rates_.end()) {
+      return it->second;
+    }
+    // Handle error case when currency abbreviation is not found
+    return 0.0;
+  }
+
+ private:
+  std::map<std::string, double> rates_;
+};
+
+class Currency {
+ public:
+  Currency() = default;
+  virtual ~Currency() = default;
+
+  virtual double ConvertedToDollars(const Cantor& cantor) const = 0;
+  virtual std::string Abbreviation() const = 0;
+  virtual double Amount() const = 0;
+};
+class Yen :   public Currency{
+private:
+double amount;
+ public:
+ Yen (double amount)
+ {
+     this->amount = amount;
+ }
+ std :: string Abbreviation()
+ {
+     return "JPY";
+ }
+ double Amount() 
+ {
+     return  amount;
+ }
+    double ConvertedToDollars (const Cantor& cantor) 
+ {
+    return amount / cantor.Rate (Abbreviation ());
+ }
+};
+
+int main() {
+    FakeUsdCantor cantor;
+    Yen yen(2.0);
+   std::cout<<yen.Amount()<<yen.Abbreviation()<<":"<<yen.ConvertedToDollars(cantor)<<"USD"<<endl;
+return 0;
+}
